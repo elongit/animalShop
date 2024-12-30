ddb794e354e21af7a750da3b8ed78203e6e88086
ossama <ossama@gmail.com>
2024-12-29 18:08:24 -0600

    added proptypes to button component

--- src/components/buttons/btn.jsx
+++ src/components/buttons/btn.jsx
@@ -1,5 +1,6 @@
 
 import { NavLink } from "react-router-dom"
+import PropTypes from 'prop-types';
 function Button({label , path}) {
   return (
    <button className="text-xl text-white bg-gradient-to-r from-rose-600 via-rose-500 to-rose-400 rounded py-3 px-8">
@@ -8,6 +9,17 @@ function Button({label , path}) {
         </NavLink>
    </button>
   )
+}
+
+Button.propTypes = {
+
+        label : PropTypes.string.isRequired,
+        path : PropTypes.string.isRequired,
+    
+      
+
+        
+ 
 }
 
 export default Button
